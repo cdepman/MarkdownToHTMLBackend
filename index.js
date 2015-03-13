6ac562e91bd983674e96b0333af089a56abcd575
Charlie Depman <cdepman@gmail.com>
2015-03-13 13:21:34 +0300

    adds socket.io and html-to-text

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 db = require('./db/db');
+var io = require('socket.io')();
+
+
+io.on('connection', function(socket){
+  console.log('<<<< socket connection established! >>>>');
+  socket.on('update', function(data){
+    console.log('Got Update!:');
+    console.log(data.data);
+    try {
+      db.setFullTimeEmailAHTML(data.data);
+    } catch(err) {
+      console.log('failed to write to database:', err);
+    }
+  });
+});
+
+io.listen(3000);
 
 db.db.on('value', function(dataSnapshot){
-
+  console.log(dataSnapshot.val());
 })
-
-
-
 
 
 db.setFullTimeEmailAMarkdown('this is NOT a test');
